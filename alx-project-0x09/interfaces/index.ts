427b1524ff0059ba03799bda0a4223c662055c9a
Ozokwelu Victor <ozokweluvic@gmail.com>
2025-01-13 18:00:06 -0800

    image generated

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,8 +1,9 @@
-import * as React from "react";
+import { ReactNode } from "react";
 
 export interface ReactComponentProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
+
 export interface GeneratedImageProps {
   imageUrl: string;
   prompt: string;
@@ -10,5 +11,9 @@ export interface GeneratedImageProps {
   height?: string;
   action: (imagePath: string) => void;
 }
+
+export type RequestProps = {
+  prompt: string;
+};
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
